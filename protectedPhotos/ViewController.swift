7b24aa87152b240e33ddc9bad14aa9ccf03a55b9
filosuf <filosuf@mail.ru>
2022-09-05 22:54:30 +0300

    Initial Commit

---
+++ protectedPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  protectedPhotos
+//
+//  Created by 1234 on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
